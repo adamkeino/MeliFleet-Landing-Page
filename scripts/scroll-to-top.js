3d04d766eb4fa87cccdf496a8e0eccb3fbf97ebc
Adam Kim <keinoadamdev@gmail.com>
2024-01-22 13:46:49 +0300

    Contact Anonymization

--- scripts/scroll-to-top.js
+++ scripts/scroll-to-top.js
@@ -3,8 +3,7 @@ let mybutton = document.getElementById("myBtn");
 // When the user scrolls down 1000px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-        mybutton.style.display = "block";
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {        mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
     }
